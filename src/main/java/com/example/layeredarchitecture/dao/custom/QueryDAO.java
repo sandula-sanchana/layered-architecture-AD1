e099081375f3c450f7f622e2e1aa7745358606f6
sandula-sanchana <sandulasanchana@gmail.com>
2025-07-16 09:33:52 +0530

    QueryDAO and CustomDTO

---
+++ src/main/java/com/example/layeredarchitecture/dao/custom/QueryDAO.java
@@ -0,0 +1,10 @@
+package com.example.layeredarchitecture.dao.custom;
+
+import com.example.layeredarchitecture.model.CustomDTO;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface QueryDAO {
+     public List<CustomDTO> getAllCustomersByOrderCount() throws SQLException ;
+}
---
+++ src/main/java/com/example/layeredarchitecture/dao/impl/QueryDAOImpl.java
@@ -0,0 +1,12 @@
+package com.example.layeredarchitecture.dao.impl;
+
+import com.example.layeredarchitecture.dao.custom.QueryDAO;
+
+import java.sql.SQLException;
+
+public class QueryDAOImpl implements QueryDAO {
+    @Override
+    public void getAllCustomersByOrderCount() throws SQLException {
+
+    }
+}
